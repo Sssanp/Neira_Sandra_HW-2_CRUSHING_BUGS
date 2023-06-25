771bc938b362f362aa7748dc102280fcca476e2b
Sssanp <121986335+Sssanp@users.noreply.github.com>
2023-06-25 19:20:43 -0400

    fixing up first bug

--- js/main.js
+++ js/main.js
@@ -3,34 +3,35 @@ const theButtons = document.querySelectorAll("#buttonHolder img"),
     puzzleBoard = document.querySelector(".puzzle-board"),
     puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
     dropZones = document.querySelectorAll(".drop-zone");
-//store the dragged piece in a global variable
-//we will need it in the handleDrop function    
+
 let draggedPiece;
 
+// Change puzzle board background image
 function changeBGImage() {
-    //console.log("changeBGImage called");
-    //url('../images/backGround0.jpg');
     puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`
 }
 
-function handleStartDrag() {
-    //console.log("Started dragging this piece:", this)
+// Start drag event 
+function handleStartDrag(e) {
     draggedPiece = this;
 }
 
+// Drag over event 
 function handleDragOver(e) {
     e.preventDefault();
-    //this will prevent the default dragover behaviour
-    //e is short for event, could be e, evt a well
     console.log("dragged over me");
 }
 
+
+
+// Drop event 
 function handleDrop(e) {
     e.preventDefault();
-    console.log("dropped something on me");
-    //this line moves the dragged piece from the left side of the board
-    //into whatever dropzone we choose.
+    if (this.children.length >= 1) {
+        return;
+    }
     this.appendChild(draggedPiece);
+    console.log("dropped something on me");
 }
 
 //event Listeners
