deba90972ed0dd430aae9cafcc2855da4a411146
Sssanp <121986335+Sssanp@users.noreply.github.com>
2023-06-25 20:07:49 -0400

    fixing up second bug

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
-//variables
+// Variables
 const theButtons = document.querySelectorAll("#buttonHolder img"),
     puzzleBoard = document.querySelector(".puzzle-board"),
     puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
-    dropZones = document.querySelectorAll(".drop-zone");
+    dropZones = document.querySelectorAll(".drop-zone"),
+    dragZone = document.querySelector(".puzzle-pieces"),
+    resetButton = document.querySelector("#resetBut");
 
 let draggedPiece;
 
 // Change puzzle board background image
 function changeBGImage() {
-    puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`
+    puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
+    resetPuzzle();
 }
 
 // Start drag event 
@@ -22,8 +25,6 @@ function handleDragOver(e) {
     console.log("dragged over me");
 }
 
-
-
 // Drop event 
 function handleDrop(e) {
     e.preventDefault();
@@ -34,11 +35,20 @@ function handleDrop(e) {
     console.log("dropped something on me");
 }
 
-//event Listeners
-theButtons.forEach(button => button.addEventListener("click", changeBGImage)); 
+// Reset puzzle by removing puzzle pieces from drop zones
+function resetPuzzle() {
+    dropZones.forEach((dropZone) => {
+        if (dropZone.children.length > 0) {
+            const puzzlePiece = dropZone.removeChild(dropZone.children[0]);
+            dragZone.appendChild(puzzlePiece);
+        }
+    });
+}
 
+// Event listeners
+theButtons.forEach(button => button.addEventListener("click", changeBGImage));
 puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
-
 dropZones.forEach(zone => zone.addEventListener("dragover", handleDragOver));
-
 dropZones.forEach(zone => zone.addEventListener("drop", handleDrop));
+resetButton.addEventListener("click", resetPuzzle);
+
